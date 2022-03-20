18aa4b853f96701e6023e662cd5b15d38cab0e55
Stuart Clark <stu@rtclark.net>
2022-03-21 10:41:58 +1100

    chore(#13): update example page

--- example/nuxt/pages/index.vue
+++ example/nuxt/pages/index.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
     <h1>DruxtLayoutParagraphs example</h1>
-    <!--<DruxtEntity mode="full" type="node--article" uuid="" />-->
+    <DruxtEntity mode="default" type="node--page" :uuid="uuid" />
   </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    uuid: '7b21da83-e2ec-4801-9758-8696340e8f46'
+  })
+}
+</script>
