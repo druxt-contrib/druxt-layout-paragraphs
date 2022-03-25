af17473f12224431b2fb511b04ccb38a387a3646
Stuart Clark <stu@rtclark.net>
2022-03-25 17:12:35 +1100

    feat(#5): add default slots for regions (#17)
    * feat(#5): add default slots for regions
    
    * chore(#5): add example layout
    
    * chore(#5): fix broken test
    
    * fix(#5): typo in slot regions
    
    * chore(#5): update tests
    
    * chore(#5): update docs

---
+++ example/nuxt/components/druxt/layout-paragraph/Twocol.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="top"><slot name="top" /></div>
+    <div class="middle">
+      <div class="first"><slot name="first" /></div>
+      <div class="second"><slot name="second" /></div>
+    </div>
+    <div class="bottom"><slot name="bottom" /></div>
+  </div>
+</template>
+
+<style scoped>
+.top {
+  font-size: 1.25rem;
+  margin-bottom: 2rem;
+}
+
+.middle {
+  display: flex;
+}
+</style>
